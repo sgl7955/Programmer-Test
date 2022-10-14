959c77aae11908de0b109625e49e29ac825977ea
Gibok <sgl7955@gmail.com>
2022-10-14 17:07:22 +0900

    qna페이지 디자인 및 버튼클릭 애니메이션 효과적용 진행상태바 구현

--- js/start.js
+++ js/start.js
@@ -1,5 +1,44 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
+const endPoint = 12;
+
+function addAnswer(answerText, qIdx) {
+    var a = document.querySelector('.answerBox');
+    var answer = document.createElement('button'); //html요소를 만들어 반환//
+    answer.classList.add('answerList') //class 값 부여//
+    answer.classList.add('my-4');
+    answer.classList.add('py-3');
+    answer.classList.add('mx-auto');
+    answer.classList.add('fadeIn');
+
+    a.appendChild(answer); //answer가 a에 속함//
+    answer.innerHTML = answerText;
+
+    answer.addEventListener("click", function() {
+        var children = document.querySelectorAll('.answerList');
+        for(let i = 0; i < children.length; i++) {
+            children[i].disabled = true;
+            main.style.WebkitAnimation = "fadeOut 0.5s";
+            main.style.animation = "fadeOut 0.5s";
+        }
+        setTimeout(() => {
+            for(let i = 0; i < children.length; i++) {
+                children[i].style.display = 'none';
+            }
+            goNext(++qIdx);
+        },450)
+    }, false);
+}
+
+function goNext(qIdx) {
+    var q = document.querySelector('.qBox');
+    q.innerHTML = qnaList[qIdx].q;
+    for(let i in qnaList[qIdx].a) {
+        addAnswer(qnaList[qIdx].a[i].answer, qIdx);
+    }
+    var status = document.querySelector('.statusBar');
+    status.style.width = (100/endPoint) * (qIdx+1) + '%';
+}
 
 function begin() {
     main.style.WebkitAnimation = "fadeOut 1s";
@@ -11,5 +50,7 @@ function begin() {
             main.style.display = "none";
             qna.style.display = "block";
         }, 450)
+        let qIdx = 0;
+        goNext(qIdx);
     }, 450);
 }
