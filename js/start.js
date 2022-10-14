1e615bf0a489fe7459ef2e6a45ab4f6cf3c90fc7
Gibok <sgl7955@gmail.com>
2022-10-14 20:15:47 +0900

    결과 페이지 전환

--- js/start.js
+++ js/start.js
@@ -1,6 +1,19 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
 const endPoint = 12;
+const result = document.querySelector("#result");
+
+function goResult() {
+    qna.style.WebkitAnimation = "fadeOut 1s";
+    qna.style.animation = "fadeOut 1s";
+    setTimeout(() => {
+        result.style.WebkitAnimation = "fadeIn 1s";
+        result.style.animation = "fadeIn 1s";
+        setTimeout(() => {
+            qna.style.display = "none";
+            result.style.display = "block";
+        }, 450)})
+}
 
 function addAnswer(answerText, qIdx) {
     var a = document.querySelector('.answerBox');
@@ -31,6 +44,11 @@ function addAnswer(answerText, qIdx) {
 }
 
 function goNext(qIdx) {
+    if(qIdx+1 === endPoint) {
+        goResult();
+        return;
+    }
+
     var q = document.querySelector('.qBox');
     q.innerHTML = qnaList[qIdx].q;
     for(let i in qnaList[qIdx].a) {
